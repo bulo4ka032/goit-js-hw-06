6e306af9fbdd217c7ac63095861fb94417f09b0b
bulo4ka032 <annagavryshkevych@gmail.com>
2023-03-13 23:09:41 +0200

    boxes fixed

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -8,7 +8,7 @@ const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
 const boxesDiv = document.querySelector("#boxes");
 
-const controls = document.querySelector("#controls");
+const controls = document.querySelector("#controls input");
 
 createBtn.addEventListener("click", amountOfBoxes);
 destroyBtn.addEventListener("click", destroyBoxes);
@@ -16,21 +16,22 @@ destroyBtn.addEventListener("click", destroyBoxes);
 let basicSize = 30;
 
 function createBoxes(amount) {
-  let boxes;
+  let boxes = "";
   for (let i = 0; i < amount; i += 1) {
     let size = basicSize + i * 10;
     boxes += `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}"></div>`;
-    console.log(boxes);
+    // console.log(boxes);
   }
   boxesDiv.insertAdjacentHTML("beforeend", boxes);
 }
 
 function amountOfBoxes() {
-  const amount = +controls.querySelector("input").value;
+  //const amount = +controls.querySelector("input").value;
+  const amount = Number(controls.value);
   console.log(amount);
   createBoxes(amount);
 }
 
 function destroyBoxes() {
-  boxes.innerHTML = "";
+  boxesDiv.innerHTML = "";
 }
